2a41353dd5d136ea18a2b1e1f9b9dc83d9baba3d
Goraksh <gorakshpatil001@gmail.com>
2026-02-03 11:11:43 +0530

    Spring boot company service

---
+++ company-service/src/main/java/com/example/companyservice/Companyservice.java
@@ -0,0 +1,11 @@
+package com.example.companyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Companyservice {
+    public static void main(String[] args) {
+        SpringApplication.run(Companyservice.class, args);
+    }
+}
